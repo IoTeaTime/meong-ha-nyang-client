4942771015f2e5a86c513bbf48d53736b65a1013
mjkwahk <mjkwahk@gmail.com>
2024-10-30 11:31:04 +0900

    fix: 불필요한 파일 제거 및 UI 수정 (#21) (KAN-88)

--- app/src/main/java/com/example/mhnfe/ui/components/deletePopup.kt
+++ app/src/main/java/com/example/mhnfe/ui/components/deletePopup.kt
@@ -1,15 +1,14 @@
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
@@ -31,6 +30,7 @@ fun DialogWithImage(
     Dialog(onDismissRequest = { onDismissRequest() }) {
         Card(
             modifier = Modifier
+                .wrapContentHeight()
                 .fillMaxWidth()
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
@@ -46,43 +46,43 @@ fun DialogWithImage(
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End // 요소를 오른쪽으로 정렬
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.x),
-                        contentDescription = null,
+                    Icon(
                         modifier = Modifier
                             .height(14.dp),
-                        contentScale = ContentScale.Fit // 비율 유지
+                        painter = painterResource(id = R.drawable.x),
+                        contentDescription = null
                     )
                 }
-                Image(
+                Icon(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(84.dp),
                     painter = painterResource(id = R.drawable.delete_dog),
                     contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth() // 너비를 가득 채우기
-                        .height(84.dp), // 높이 설정
-                    contentScale = ContentScale.Fit // 비율 유지
                 )
                 Text(
+                    modifier = Modifier.padding(PaddingValues(top = 17.dp)),
                     text = buildAnnotatedString {
                         append("회원탈퇴")
                         withStyle(style = SpanStyle(color = mainBlack)) {
                             append("를")
                         }
                     },
-                    modifier = Modifier.padding(PaddingValues(top = 17.dp)),
                     color = Color.Red
                 )
                 Text(
-                    text = "진행하시겠습니까?",
                     modifier = Modifier
                         .padding(PaddingValues(bottom = 30.dp)),
+                    text = "진행하시겠습니까?",
                     color = mainBlack
                 )
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Center,
                 ) {
                     TextButton(
@@ -110,6 +110,7 @@ fun NewQuizPreview() {
     Column(
         modifier = Modifier
             .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         DialogWithImage(onConfirmation = {}) { }
