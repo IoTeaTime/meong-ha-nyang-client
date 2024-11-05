5c62989f1187e07c521f070f21f3f05037287a67
kimmatches <peach3139@gmail.com>
2024-11-05 15:33:31 +0900

    feat : 앱 구조 설정 (#54) (KAN-127)

--- app/src/main/java/com/example/mhnfe/ui/screens/auth/SelectScreen.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/auth/SelectScreen.kt
@@ -8,17 +8,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.mhnfe.R
-import com.example.mhnfe.ui.components.middleButton
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.ui.graphics.Color
 import com.example.mhnfe.ui.theme.mainYellow
 
 @Composable
-fun HomeScreen(
+fun SelectScreen(
     onBackClick: () -> Unit
 ) {
     var showQrDialog by remember { mutableStateOf(false) }
