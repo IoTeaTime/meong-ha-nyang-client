887e6fd1036db56c6adb8f36b14aa0dc7ad83650
ywonchae1 <ywonchae62@gmail.com>
2024-11-14 14:58:02 +0900

    feat: FCM 토큰 발급 및 서버로 전송 구현 (#104)

---
+++ app/src/main/java/com/example/mhnfe/data/model/request/RequestFcmToken.kt
@@ -0,0 +1,10 @@
+package com.example.mhnfe.data.model.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestFcmToken(
+    @SerialName("token")
+    val token: String
+)
