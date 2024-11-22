657613b05f362a547dff6927ae5a25419e8e8fe7
mjkwahk <mjkwahk@gmail.com>
2024-11-22 17:49:27 +0900

    fix: textBox파일 warningText 수정 (#109)

--- app/src/main/java/com/example/mhnfe/ui/components/TextBox.kt
+++ app/src/main/java/com/example/mhnfe/ui/components/TextBox.kt
@@ -153,7 +153,7 @@ fun MainTextBox(
                     tint = mainRed
                 )
                 Text(
-                    text = "warningText",
+                    text = warningText,
                     color = mainRed,
                     style = Typography.bodySmall.copy(fontSize = 12.sp)
                 )
