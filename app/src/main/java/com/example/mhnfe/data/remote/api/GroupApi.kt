57b8c10e88fa3fb05376ee58464075d6545f0b4d
kimmatches <peach3139@gmail.com>
2024-11-27 16:16:47 +0900

    feat: 그룹 정보api 수정 (#156)

--- app/src/main/java/com/example/mhnfe/data/remote/api/GroupApi.kt
+++ app/src/main/java/com/example/mhnfe/data/remote/api/GroupApi.kt
@@ -2,8 +2,10 @@ package com.example.mhnfe.data.remote.api
 
 
 import com.example.mhnfe.data.remote.response.CreateGroupRequest
+import com.example.mhnfe.data.remote.response.GroupInfoResponse
 import com.example.mhnfe.data.remote.response.GroupResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -13,4 +15,9 @@ interface GroupApi {
         @Header("Authorization") authToken: String,
         @Body request: CreateGroupRequest
     ): GroupResponse
+
+    @GET("/api/group/info-list")
+    suspend fun getGroupInfo(
+        @Header("Authorization") authToken: String
+    ): GroupInfoResponse
 }
--- app/src/main/java/com/example/mhnfe/data/remote/response/GroupResponse.kt
+++ app/src/main/java/com/example/mhnfe/data/remote/response/GroupResponse.kt
@@ -3,20 +3,36 @@ package com.example.mhnfe.data.remote.response
 import com.example.mhnfe.data.remote.request.GroupRequest
 import kotlinx.serialization.Serializable
 
-@Serializable
 data class GroupResponse(
     val result: GroupRequest,
     val body: Group
 )
 
-@Serializable
 data class Group(
     val groupId: Int,
     val groupName: String,
     val createdAt: String
 )
-@Serializable
+
 data class CreateGroupRequest(
     val thingId: String
 )
 
+data class GroupInfoResponse(
+    val result: GroupRequest,
+    val body: GroupInfo
+)
+
+data class GroupInfo(
+    val groupId: Long,
+    val groupName: String,
+    val createdAt: String,
+    val cctv: List<CctvInfo>
+)
+
+data class CctvInfo(
+    val cctvId: Long,
+    val cctvNickname: String,
+    val thingId: String,
+    val kvsChannelName: String,
+)
