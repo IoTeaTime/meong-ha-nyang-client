0cfda708edb83ece4beba934598340b8616654a6
dlawlstn1616 <a01047181185@gmail.com>
2024-11-27 13:53:55 +0900

    feat: UserRepository divide to UserRepository and UserRepositoryImpl (#153)

---
+++ app/src/main/java/com/example/mhnfe/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.example.mhnfe.data.repository
+
+import com.example.mhnfe.domain.repository.UserRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor() : UserRepository{
+
+}
