446bd635731fc39b6e94a35fac7d925f30da05d2
kimmatches <peach3139@gmail.com>
2024-11-27 16:50:53 +0900

    feat: 디바이스 정보 페이지 api 연결 (#156)

--- app/src/main/java/com/example/mhnfe/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/mhnfe/ui/navigation/AppNavigation.kt
@@ -262,10 +262,10 @@ fun MainContent(
                 composable(
                     route = NavRoutes.Monitoring.DeviceInformation.route,
                     arguments = listOf(
-                        navArgument("cctvId") { type = NavType.StringType }
+                        navArgument("cctvId") { type = NavType.LongType }
                     )
                 ) { backStackEntry ->
-                    val cctvId = backStackEntry.arguments?.getString("cctvId") ?: return@composable
+                    val cctvId = backStackEntry.arguments?.getLong("cctvId") ?: return@composable
                     DeviceInfoScreen(
                         cctvId = cctvId,
                         navController = bottomNavController
