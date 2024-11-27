6ff312e31c4a331096076dc990392288b4719e61
dlawlstn1616 <a01047181185@gmail.com>
2024-11-27 14:30:11 +0900

    fix: 자동로그인 로직 변경 (#153)

--- app/src/main/java/com/example/mhnfe/ui/screens/auth/main/MainScreen.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/auth/main/MainScreen.kt
@@ -12,10 +12,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.datastore.core.DataStore
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mhnfe.R
+import com.example.mhnfe.data.remote.request.LoginRequest
 import com.example.mhnfe.data.repository.AuthRepository
 import com.example.mhnfe.di.UserType
 import com.example.mhnfe.ui.components.MiddleButton
@@ -35,7 +37,7 @@ fun MainScreen(
 
     // 자동 로그인 로직
     LaunchedEffect(Unit) {
-        mainViewModel.autoLogin(savedId, savedPassword,
+        mainViewModel.autoLogin(
             onSuccess = {
                 // 자동 로그인 성공 -> 다음 화면으로 이동
                 navController.navigate(NavRoutes.Auth.Select.route) {
