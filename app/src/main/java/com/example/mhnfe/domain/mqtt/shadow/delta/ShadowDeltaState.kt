2a6535c1b30d63152eaa84d50210da6052ca6a18
OziinG <dlajiin@gmail.com>
2024-11-28 17:14:56 +0900

    fix: JSON Parsing 문제 해결 (#163)

--- app/src/main/java/com/example/mhnfe/domain/mqtt/shadow/delta/ShadowDeltaState.kt
+++ app/src/main/java/com/example/mhnfe/domain/mqtt/shadow/delta/ShadowDeltaState.kt
@@ -4,5 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ShadowDeltaState(
-    val delta: ShadowContent
+    val delta: ShadowContent? = null,
+    val reported: ShadowContent? = null
 )
