0a69551f33ceb74c295cb1d51719e248a530cebf
bestKUFO <best140804@gmail.com>
2024-11-29 09:47:39 +0900

    del: openCV connect fail

--- opencv/build/intermediates/cxx/Debug/41kn1z11/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\r2com\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\r2com\\Desktop\\meong-ha-nyang-client\\opencv\\.cxx\\Debug\\41kn1z11\\x86_64" ^
-  opencv_jni_shared
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,4 +17,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "mhnFE"
 include(":app")
-include(":opencv")
