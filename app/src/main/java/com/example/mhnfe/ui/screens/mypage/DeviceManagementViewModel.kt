a11360c451daa72b6248d2d36c38d58afd9bbd1d
ywonchae1 <ywonchae62@gmail.com>
2024-11-29 13:30:43 +0900

    fix: Api 응답 에러 핸들링 수정 (#172)

--- app/src/main/java/com/example/mhnfe/ui/screens/mypage/DeviceManagementViewModel.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/mypage/DeviceManagementViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mhnfe.data.remote.response.AccessToken
 import com.example.mhnfe.data.remote.response.DeleteDeviceResponse
-import com.example.mhnfe.data.remote.response.Result
 import com.example.mhnfe.domain.repository.DeviceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,9 +14,11 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
+import kotlinx.serialization.json.Json
+import org.json.JSONObject
 import retrofit2.Response
 import javax.inject.Inject
+
 
 @HiltViewModel
 class DeviceManagementViewModel @Inject constructor(
@@ -27,21 +28,16 @@ class DeviceManagementViewModel @Inject constructor(
     private val _errorMessage = MutableStateFlow<String?>(null)
     val errorMessage: StateFlow<String?> = _errorMessage
 
-    private fun mapErrorMessage(code: Int, message: String, description: String?): String {
-        return when (message) {
-            "NOT FOUND" -> "회원 정보를 찾을 수 없습니다. 다시 확인해주세요."
-            "BAD REQUEST" -> "비밀번호가 일치하지 않습니다. 다시 입력해주세요."
-            else -> "알 수 없는 오류가 발생했습니다.\n에러 코드: $code\n$description"
-        }
-    }
-
     fun deleteDevice(cctvId: Long) {
         viewModelScope.launch {
             val token = accessTokenDataStore.data.map { it.accessToken }.first()
             val response: Response<DeleteDeviceResponse>?
             response = deviceRepository.deleteDevice(token, cctvId)
             if(!response.isSuccessful) {
-                _errorMessage.value = "기기 삭제 실패 " + response.code()
+                val jsonObject = JSONObject(response.errorBody()!!.string())
+                val errorBody = Json.decodeFromString<DeleteDeviceResponse>(jsonObject.toString())
+
+                _errorMessage.value = errorBody.result.description
                 Log.e(TAG,"Error: " + _errorMessage.value)
             }
         }
