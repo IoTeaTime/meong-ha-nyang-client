11194b4b3c7f557908d3126772ca49b05b73b3e2
bestKUFO <best140804@gmail.com>
2024-11-29 11:34:14 +0900

    feat: send Coordinates(location) with Json form

--- app/src/main/java/com/example/mhnfe/ui/screens/monitoring/kvs/AiViewModel.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/monitoring/kvs/AiViewModel.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mhnfe.domain.ai.BoundingBoxUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,28 +27,21 @@ class AiViewModel @Inject constructor() : ViewModel() {
 
     fun detectEvent(onResult: (String) -> Unit, onPayloadReady: (String) -> Unit) {
         viewModelScope.launch {
-            // input "detect" or "category"
-            val aiResult = "dog" // TODO. Replace with actual AI processing logic
+            val aiResult = "dog"
+            val coordinatesJson = BoundingBoxUtils.boundingBoxJson()
+
             val payload = """
-            {
-                "trackingId": 1,
-                "timestamp": ${System.currentTimeMillis() / 1000}, 
-                "objectType": "$aiResult", 
-                "location": { 
-                    "x1": 30, 
-                    "y1": 0,
-                    "x2": 30, 
-                    "y2": 700,
-                    "x3": 1200,
-                    "y3": 700,
-                    "x4": 1200,
-                    "y4": 0
-                }
-            }
-            """.trimIndent()
+        {
+            "trackingId": 1,
+            "timestamp": ${System.currentTimeMillis() / 1000}, 
+            "objectType": "$aiResult", 
+            "coordinates": $coordinatesJson
+        }
+        """.trimIndent()
             // Callback
             onResult(aiResult)
             onPayloadReady(payload)
         }
     }
+
 }
