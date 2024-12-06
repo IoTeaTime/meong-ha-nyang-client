6c979e0ac7aba1740477004f91bf413cb774a9f0
bestKUFO <best140804@gmail.com>
2024-12-06 17:09:06 +0900

    feature: AI event data send to Iot (#249) (KAN-24)

--- app/src/main/java/com/example/mhnfe/domain/ai/yolo/HandleDetection.kt
+++ app/src/main/java/com/example/mhnfe/domain/ai/yolo/HandleDetection.kt
@@ -3,15 +3,9 @@ package com.example.mhnfe.domain.ai.yolo
 import android.util.Log
 
 class HandleDetection {
-
-    private val TAG = "HandleDetection"
-
     fun handleDetectionResults(boundingBoxes: List<BoundingBox>): List<BoundingBox> {
         val filteredBoxes = boundingBoxes.filter {
             it.objectName == "dog" || it.objectName == "cat" || it.objectName == "person"
-        }
-        filteredBoxes.forEach { box ->
-            Log.d(TAG, "Detected ${box.objectName} with confidence: ${box.cnf}")
         }
         return filteredBoxes
     }
