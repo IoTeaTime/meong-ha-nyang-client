9195dac30fed686d35a3501804cac507fd0f6903
OziinG <dlajiin@gmail.com>
2024-12-07 12:59:10 +0900

    fix: 1280 * 720 위치로 변환 (#261)

--- app/src/main/java/com/example/mhnfe/domain/ai/BoundingBoxUtils.kt
+++ app/src/main/java/com/example/mhnfe/domain/ai/BoundingBoxUtils.kt
@@ -15,17 +15,17 @@ object BoundingBoxUtils {
     }
 
     // 각 박스의 좌표를 반환하는 메서드
-    fun getCoordinates(boundingBoxes: List<BoundingBox>): List<Map<String, Float>> {
+    fun getCoordinates(boundingBoxes: List<BoundingBox>): List<Map<String, Int>> {
         return boundingBoxes.map { box ->
             mapOf(
                 "x1" to box.x1,
                 "y1" to box.y1,
                 "x2" to box.x2,
-                "y2" to box.y1,
+                "y2" to box.y2,
                 "x3" to box.x1,
                 "y3" to box.y2,
                 "x4" to box.x2,
-                "y4" to box.y2
+                "y4" to box.y1
             )
         }
     }
@@ -33,4 +33,18 @@ object BoundingBoxUtils {
     // 이벤트 발생 여부를 결정하는 메서드
     fun shouldTriggerEvent(lastEventTime: Long, eventDelayMillis: Long): Boolean =
         (System.currentTimeMillis() - lastEventTime) >= eventDelayMillis
+
+    // 좌표를 단일 JSON 객체로 변환하는 메서드
+    fun getFirstCoordinatesAsJson(boundingBoxes: List<BoundingBox>): String {
+        return boundingBoxes.firstOrNull()?.let { box ->
+            """
+            {
+                "x1": ${box.x1}, "y1": ${box.y1},
+                "x2": ${box.x2}, "y2": ${box.y2},
+                "x3": ${box.x1}, "y3": ${box.y2},
+                "x4": ${box.x2}, "y4": ${box.y1}
+            }
+            """.trimIndent()
+        } ?: "{}"
+    }
 }
--- app/src/main/java/com/example/mhnfe/domain/mqtt/MqttViewModel.kt
+++ app/src/main/java/com/example/mhnfe/domain/mqtt/MqttViewModel.kt
@@ -308,32 +308,28 @@ class MqttViewModel @Inject constructor(
         trackingId: Int,
         objectName: String,
         confidence: Float,
-        coordinates: List<Map<String, Float>>
+        coordinates: List<Map<String, Int>>
     ) {
-        val coordinatesJson = coordinates.firstOrNull()?.let { coord ->
+        val payload = coordinates.firstOrNull().let { coord ->
             """
         {
-            "x1": ${coord["x1"]}, "y1": ${coord["y1"]},
-            "x2": ${coord["x2"]}, "y2": ${coord["y2"]},
-            "x3": ${coord["x3"]}, "y3": ${coord["y3"]},
-            "x4": ${coord["x4"]}, "y4": ${coord["y4"]}
+            "trackingId": $trackingId,
+            "timestamp": ${System.currentTimeMillis() / 1000},
+            "objectType": "$objectName",
+            "confidence": $confidence,
+            "coordinates": {
+                "x1": ${coord!!["x1"]}, "y1": ${coord["y1"]},
+                "x2": ${coord["x2"]}, "y2": ${coord["y2"]},
+                "x3": ${coord["x3"]}, "y3": ${coord["y3"]},
+                "x4": ${coord["x4"]}, "y4": ${coord["y4"]}
+            }
         }
         """.trimIndent()
-        } ?: "{}"
-
-        val payload =
-            """
-            {
-                "trackingId": $trackingId,
-                "timestamp": ${System.currentTimeMillis() / 1000}, 
-                "objectType": "$objectName",
-                "confidence": $confidence,
-                "coordinates": $coordinatesJson
-           }
-        """.trimIndent()
+        }
 
         publish("/mhn/event/detect/things/$thingId", payload)
     }
+
 
     //shadow
     fun subscribeShadowWithPayload(thingId: String, data: (ReportedData?) -> Unit) {
