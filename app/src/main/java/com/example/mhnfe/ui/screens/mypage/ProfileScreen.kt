09b05c06cdd67f6f628c87cfc086bf71638dff10
mjkwahk <mjkwahk@gmail.com>
2024-12-07 16:44:54 +0900

    fix: 아이디값 수정 및 icon 이미지 변경 (#276)

--- app/src/main/java/com/example/mhnfe/ui/screens/mypage/ProfileScreen.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/mypage/ProfileScreen.kt
@@ -228,7 +228,7 @@ fun ProfileScreen(
                                     style = Typography.bodyMedium,
                                     color = mainBlack
                                 )
-                                profileResponse?.body?.member?.id?.toString()?.let {
+                                profileResponse?.body?.member?.email?.let {
                                     Text(
                                         modifier = modifier,
                                         text = it,
