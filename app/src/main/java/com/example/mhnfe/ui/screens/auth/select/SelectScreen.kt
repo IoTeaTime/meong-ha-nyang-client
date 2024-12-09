86203567f16127d31ffa3250822cbda1ed3989de
DDU <a01047181185@gmail.com>
2024-12-09 22:45:47 +0900

    refactor: screens/auth (#290)

--- app/src/main/java/com/example/mhnfe/ui/screens/auth/select/SelectScreen.kt
+++ app/src/main/java/com/example/mhnfe/ui/screens/auth/select/SelectScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mhnfe.ui.screens.auth.select
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
