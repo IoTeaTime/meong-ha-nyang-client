0b510090018369705a646225598ac692fc1a883d
dlawlstn1616 <a01047181185@gmail.com>
2024-12-11 12:38:54 +0900

    refactor: ui/theme (#290)

--- app/src/main/java/com/example/mhnfe/ui/theme/Type.kt
+++ app/src/main/java/com/example/mhnfe/ui/theme/Type.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.mhnfe.R
 
-
 // 사용 예시 Text(style = Typography.bodyMedium, text = "글씨")
 
 val Roboto = FontFamily(
